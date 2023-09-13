f785c2409c6d8f3f33e0336428ff1238913ec5e6
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-13 16:19:43 +0200

    add ticket price

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,38 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//RACCOLGO I DATI
+		System.out.print("Scrivi quanti km devi fare: ");
+		int km = sc.nextInt();
+		
+		System.out.print("Quanti anni hai?:  ");
+		byte età = sc.nextByte();
+		
+		//CHIUDO LO SCANNER
+		sc.close();
+		
+		//CALCOLO PREZZO
+		double prezzoPieno = 0.21 * km;
+		double prezzoScontato = prezzoPieno;
+		
+		//CALCOLO SCONTO
+		if(età < 18) {
+			prezzoScontato *= 0.8;
+			
+		} else if(età >= 65) {
+			prezzoScontato *= 0.6;
+			
+		}
+		
+		//OUTPUT PER L'UTENTE
+		System.out.println("Il prezzo del tuo biglietto è: " + (String.format("%.02f", prezzoScontato)) + " €");
+	}
 
 }
